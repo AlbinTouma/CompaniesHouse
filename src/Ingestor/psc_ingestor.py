bb32aa1328cc3b1a55298ab8600dc8d57dce4236
Albin Touma <albin.touma@gmail.com>
2025-12-21 12:55:07 +0100

    Ingestor tqdm progress bar added

--- src/Ingestor/psc_ingestor.py
+++ src/Ingestor/psc_ingestor.py
@@ -3,11 +3,17 @@ from src.schemas.psc import PscRead
 from sqlmodel import Session
 from src.config import engine
 import json
+from tqdm import tqdm
+
 
 with open('psc.txt', 'r') as file:
+    total_rows = sum(1 for _ in file)
+    file.seek(0)  # reset to start
+        
     with Session(engine) as session:
+           
         pscsql_list: list[PSC] = []
-        for line in file:
+        for line in tqdm(file, total=total_rows, desc="Ingest PSC"):
 
             psc_dict = json.loads(line)
             psc = PscRead.model_validate(psc_dict['data'])
@@ -25,5 +31,12 @@ with open('psc.txt', 'r') as file:
                     session.merge(psc)
                 session.commit()
                 pscsql_list.clear()
-                print("Added")
-                break  # Just process the first line for demonstration purposes
+                break
+        
+        if pscsql_list:
+            for psc in pscsql_list:
+                session.merge(psc)
+            session.commit()
+            pscsql_list.clear()
+
+        print("INGESTION PROCESS COMPLETE")    
